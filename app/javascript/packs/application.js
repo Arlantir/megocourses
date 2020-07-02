9abb096b106a691af1b89e7b16695f0d77e24e2b
Arlantir <ror@worldoflosttime.ru>
2020-07-03 01:40:53 +0300

    setting project

--- app/javascript/packs/application.js
+++ app/javascript/packs/application.js
@@ -1,7 +1,8 @@
-require("@rails/ujs").start();
-require("turbolinks").start();
-require("@rails/activestorage").start();
-require("channels");
-require("bootstrap/dist/js/bootstrap");
+require("@rails/ujs").start()
+require("turbolinks").start()
+require("@rails/activestorage").start()
+require("channels")
+require("bootstrap/dist/js/bootstrap")
 
-import '../stylesheets/application';
+import '../stylesheets/application'
+import "@fortawesome/fontawesome-free/js/all"
