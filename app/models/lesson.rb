fbf2f9b99281e6ae3d548852d1e5ec78884bbb1f
Arlantir <ror@worldoflosttime.ru>
2020-08-13 01:36:39 +0300

    add public_activity for lessons and small refact

--- app/models/lesson.rb
+++ app/models/lesson.rb
@@ -1,9 +1,15 @@
 class Lesson < ApplicationRecord
   extend FriendlyId
+  include PublicActivity::Model
+  tracked owner: Proc.new{ |controller, _model| controller.current_user }
 
   belongs_to :course
 
   validates :title, :content, :course, presence: true
 
   friendly_id :title, use: :slugged
+
+  def to_s
+    title
+  end
 end
